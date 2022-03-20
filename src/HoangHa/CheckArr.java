1b1e21faa8a7e86e2e40b4b6e0e5c259eff559a3
Hoanghahiha <81789681+Hoanghahiha@users.noreply.github.com>
2022-03-20 10:38:13 +0700

    cn

--- src/HoangHa/CheckArr.java
+++ src/HoangHa/CheckArr.java
@@ -11,14 +11,22 @@ public class CheckArr {
             System.out.printf("a[%d] = ", i);
             arr[i] = scanner.nextInt();
         }
-        System.out.print("Các phần tử của mảng: ");
-        show(arr);
-    }
-
-    public static void show(int[] arr) {
+        System.out.print("Các phần tử của mảng: \n");
         for (int i = 0; i < arr.length; i++) {
-            System.out.print(arr[i] + " ");
+            System.out.print(arr[i] + " \n");
+        }
+        System.out.println("Các số nguyên tố: ");
+        int count;
+        for(int i = 0; i < 10 ; i++) {
+            count = 0;
+            for(int a = 2; a <= arr[i]; a++) {
+                if(arr[i] % a == 0) {
+                    count += 1;
+                }
+            }
+            if(count == 1) {
+                System.out.println(arr[i]);
+            }
         }
     }
-
 }
--- src/HoangHa/Main.java
+++ src/HoangHa/Main.java
@@ -4,54 +4,55 @@ public class Main {
 
     public static void main(String[] args) {
         // write your code here
-        System.out.println("lalala");
+        System.out.println("Hello world!");
         int x = 10;
         double pi = 3.14159;
         char h = 'h';
         boolean t = true;
-        String s = "T2018M";
+        String s = "T2108M";
         s = s + " SEM 2";
         System.out.println(s);
-        if (x >= 10) {
+        if(x>=10){
             System.out.println("x >= 10");
-        } else {
-            System.out.println("Not Found");
+        }else{
+            System.out.println("Not found");
         }
-        switch (h) {
-            case 'a':
-                System.out.println("AA");
-                break;
-            case 'h':
-                System.out.println("HH");
-                break;
-            default:
-                System.out.println("Not Found");
+        switch (h){
+            case 'a':System.out.println("AA");break;
+            case 'h':System.out.println("HH");break;
+            default:System.out.println("NOT FOUND");
         }
-        for (int i = 0; i < 10; i++) {
-            System.out.println("i= " + i);
+        for (int i=0;i<10;i++){
+            System.out.println("i= "+i);
         }
-        System.out.println("Nhap 1 so tu ban phim");
+        System.out.println("NHap 1 so tu ban phim:");
         Scanner sc = new Scanner(System.in);
         int n;
-        n = sc.nextInt();
+        n = sc.nextInt();// scanf("%d",&n);
         sc.nextLine();
-        System.out.println("So vua nhap:" + n);
+        System.out.println("So vua nhap:"+n);
         String s1 = sc.nextLine();
-        System.out.println("String: " + s1);
+        System.out.println("String: "+s1);
+
         //array
-        int[] arr = new int[10]; // giong int arr[10]
+        int[] arr = new int[10];// int arr[10]
         arr[0] = 13;
         arr[1] = 15;
-        for (int i = 0; i < 10; i++) {
-            arr[1] = i * 2 + 1;
+        for(int i=0;i<10;i++){
+            arr[i] = i*2+1;
         }
-        for (int i = 0; i < 10; i++) {
+        for(int i=0;i<10;i++){
             System.out.println(arr[i]);
         }
-        //for-each
-        for (int a : arr) {
-            System.out.println(a);
+        // for-each
+        for(int r : arr){
+            System.out.println(r);
         }
+
+        String[] products = new String[5];
+        for(String product:products){
+
+        }
+
     }
 }
-
